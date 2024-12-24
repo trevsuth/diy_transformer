c235119f9297abb83dc6026304109451c95c53d6
trevsuth <trevor.sutherland@gmail.com>
2024-12-24 09:10:18 -0500

    added layer normalization

---
+++ diy_transformer/transformer.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class ImputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        """init instance of the InputEmbedding class
+
+        Args:
+            d_model (int): dimensionality of the embeddings
+            vocab_size (int): total number of unique tokens
+        """
+        super().__init__() #calls construuctor of nn.Module
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        """Defines the forward pass of the module"""
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq = seq
+        self.dropout = nn.Dropout(dropout)
+
+        # Createa matrix of shape (seq, d_model)
+        pe = torch.zeros(seq, d_model)
+
+        # Create a vector of shape (seq)
+        position = torch.arange(0, seq, dtype=torch.float).unsqueeze(1) # (seq, 1)
+
+        # Create a vector of shape (d_model)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_modell / 2)
+
+        # Apply sine to even indices
+        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * 10000 ** (2i / d_model))
+
+        # Apply cosine to odd indices
+        pe[:, 1::2] = torch.cos(position * div_term) # sin(position * 10000 ** (2i / d_model))
+
+        # Add a batch dimensions to original positional encoding
+        pe = pe.unsqueeze(0) # (1, seq, d_model)
+
+        # Register the positional encoding as a buffer
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq, d_model)
+        return self.dropout(x)
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha (multiplicative) is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias (addative) is a learnavle parameter
+
+    def forward(self, x):
+        # x: (batch, seq, hidden_size)
+        # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim=True) # (batch, seq, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
