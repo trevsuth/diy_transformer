bbc2620e9655c7506007a87592d375f4981c06e9
trevsuth <trevor.sutherland@gmail.com>
2024-12-24 15:26:07 -0500

    added feedforwardblock

--- diy_transformer/transformer.py
+++ diy_transformer/transformer.py
@@ -70,3 +70,16 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim=True) # (batch, seq, 1)
         # eps is to prevent dividing by zero or when std is very small
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForwardBlock(nn.Modle):
+    
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+    
+    def forward(self, x):
+        # (batch, seq, d_model) --> (batch, seq, d_ff) --> (batch, seq, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
