0ec14af57b0d6b458655250775964d3331326f4a
trevsuth <trevor.sutherland@gmail.com>
2024-12-25 07:28:28 -0500

    added encoder

--- diy_transformer/transformer.py
+++ diy_transformer/transformer.py
@@ -4,9 +4,9 @@ import math
 
 
 class ImputEmbeddings(nn.Module):
-    
+
     def __init__(self, d_model: int, vocab_size: int):
-        super().__init__() #calls construuctor of nn.Module
+        super().__init__()  # calls construuctor of nn.Module
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
@@ -17,7 +17,7 @@ class ImputEmbeddings(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    
+
     def __init__(self, d_model: int, seq: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
@@ -28,75 +28,87 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(seq, d_model)
 
         # Create a vector of shape (seq)
-        position = torch.arange(0, seq, dtype=torch.float).unsqueeze(1) # (seq, 1)
+        position = torch.arange(0, seq, dtype=torch.float).unsqueeze(1)  # (seq, 1)
 
         # Create a vector of shape (d_model)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) # (d_modell / 2)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )  # (d_modell / 2)
 
         # Apply sine to even indices
-        pe[:, 0::2] = torch.sin(position * div_term) # sin(position * 10000 ** (2i / d_model))
+        pe[:, 0::2] = torch.sin(
+            position * div_term
+        )  # sin(position * 10000 ** (2i / d_model))
 
         # Apply cosine to odd indices
-        pe[:, 1::2] = torch.cos(position * div_term) # sin(position * 10000 ** (2i / d_model))
+        pe[:, 1::2] = torch.cos(
+            position * div_term
+        )  # sin(position * 10000 ** (2i / d_model))
 
         # Add a batch dimensions to original positional encoding
-        pe = pe.unsqueeze(0) # (1, seq, d_model)
+        pe = pe.unsqueeze(0)  # (1, seq, d_model)
 
         # Register the positional encoding as a buffer
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
 
     def forward(self, x):
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False) # (batch, seq, d_model)
+        x = x + (self.pe[:, : x.shape[1], :]).requires_grad_(
+            False
+        )  # (batch, seq, d_model)
         return self.dropout(x)
 
 
 class LayerNormalization(nn.Module):
 
-    def __init__(self, features: int, eps:float=10**-6) -> None:
+    def __init__(self, features: int, eps: float = 10**-6) -> None:
         super().__init__()
         self.eps = eps
-        self.alpha = nn.Parameter(torch.ones(features)) # alpha (multiplicative) is a learnable parameter
-        self.bias = nn.Parameter(torch.zeros(features)) # bias (addative) is a learnavle parameter
+        self.alpha = nn.Parameter(
+            torch.ones(features)
+        )  # alpha (multiplicative) is a learnable parameter
+        self.bias = nn.Parameter(
+            torch.zeros(features)
+        )  # bias (addative) is a learnavle parameter
 
     def forward(self, x):
         # x: (batch, seq, hidden_size)
         # Keep the dimension for broadcasting
-        mean = x.mean(dim = -1, keepdim = True) # (batch, seq, 1)
+        mean = x.mean(dim=-1, keepdim=True)  # (batch, seq, 1)
         # Keep the dimension for broadcasting
-        std = x.std(dim = -1, keepdim=True) # (batch, seq, 1)
+        std = x.std(dim=-1, keepdim=True)  # (batch, seq, 1)
         # eps is to prevent dividing by zero or when std is very small
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
 
 class FeedForwardBlock(nn.Module):
-    
+
     def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
         super().__init__()
-        self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+        self.linear_1 = nn.Linear(d_model, d_ff)  # w1 and b1
         self.dropout = nn.Dropout(dropout)
-        self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
-    
+        self.linear_2 = nn.Linear(d_ff, d_model)  # w2 and b2
+
     def forward(self, x):
         # (batch, seq, d_model) --> (batch, seq, d_ff) --> (batch, seq, d_model)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
 
 class MultiHeadAttentionBlock(nn.Module):
-    
+
     def __init__(self, d_model: int, h: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
-        self.h = h # number of heads
+        self.h = h  # number of heads
         # Make sure that d_model is divisable by h
         assert d_model % h == 0, "d_model is not divisable by h"
-        
-        self.d_k = d_model // h # Dimension of vector seen by each head
-        self.w_q = nn.Linear(d_model, d_model, bias=False) # Wq
-        self.w_k = nn.Linear(d_model, d_model, bias=False) # Wk
-        self.w_v = nn.Linear(d_model, d_model, bias=False) # Wv
-        self.w_o = nn.Linear(d_model, d_model, bial=False) # Wo
+
+        self.d_k = d_model // h  # Dimension of vector seen by each head
+        self.w_q = nn.Linear(d_model, d_model, bias=False)  # Wq
+        self.w_k = nn.Linear(d_model, d_model, bias=False)  # Wk
+        self.w_v = nn.Linear(d_model, d_model, bias=False)  # Wv
+        self.w_o = nn.Linear(d_model, d_model, bial=False)  # Wo
         self.dropout = nn.Dropout(dropout)
-        
+
     @staticmethod
     def attention(query, key, value, mask, dropout: nn.Dropout):
         d_k = query.shape[-1]
@@ -105,37 +117,46 @@ class MultiHeadAttentionBlock(nn.Module):
         attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
         if mask is not None:
             # Write a very low value (indicating -inf) to the positions where mask == 0
-            attention_scores.masked_fill_(mask == 0, -1e)
-        attention_scores = attention_scores.softmax(dim=1) # (batch, h, seq, seq) # Apply softmax
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(
+            dim=1
+        )  # (batch, h, seq, seq) # Apply softmax
         if dropout is not None:
             attention_scores = dropout(attention_scores)
         # (batch, h, seq, seq) --> (batch, h, seq, d_k)
         # return attention scores which can be used for vizulation
         return (attention_scores @ value), attention_scores
-    
+
     def forward(self, q, k, v, mask):
-        query = self.w_q(q) # (batch, seq, d_model) --> (batch, seq, d_model)
-        key = self.w_k(k) # (batch, sed, d_model) --> (batch, seq, d_model)
-        value = self.w_v(v) # (batch, seq, d_model) --> (batch, seq, d_model)
-        
+        query = self.w_q(q)  # (batch, seq, d_model) --> (batch, seq, d_model)
+        key = self.w_k(k)  # (batch, sed, d_model) --> (batch, seq, d_model)
+        value = self.w_v(v)  # (batch, seq, d_model) --> (batch, seq, d_model)
+
         # (batch, seq, d_model) --> (batch, seq, h, d_k) --> (batch, h, seq, d_k)
-        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )
         key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
-        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
-        
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(
+            1, 2
+        )
+
         # Calculate Attention
-        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
-        
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(
+            query, key, value, mask, self.dropout
+        )
+
         # Combine all the heads together
         # (batch, h, seq, d_k) --> (batch, seq, h, d_k) --> (batch, seq, d_model)
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
-        
+
         # Multiply by Wo
         # (batch, seq, d_model) --> (batch, seq, d_model)
         return self.w_o(x)
-    
+
+
 class ResidualConnection(nn.Module):
-    
+
     def __init__(self, features: int, dropout: float) -> None:
         super().__init__()
         self.dropout = nn.Dropout(dropout)
@@ -144,15 +165,38 @@ class ResidualConnection(nn.Module):
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
 
+
 class EncoderBlock(nn.Module):
-    
-    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+
+    def __init__(
+        self,
+        features: int,
+        self_attention_block: MultiHeadAttentionBlock,
+        feed_forward_block: FeedForwardBlock,
+        dropout: float,
+    ) -> None:
         super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connections = nn.Module([ResidualConnection(features, dropout) for _ in range(2)])
+        self.residual_connections = nn.Module(
+            [ResidualConnection(features, dropout) for _ in range(2)]
+        )
 
     def forward(self, x, src_mask):
-        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[0](
+            x, lambda x: self.self_attention_block(x, x, x, src_mask)
+        )
         x = self.residual_connections[1](x, self.feed_forward_block)
         return x
+
+class Encoder(nn.Module):
+    
+    def __init__(self, features: int, layers: nn.Module) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
